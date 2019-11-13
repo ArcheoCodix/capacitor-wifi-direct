ca9b30f1360ca2385dae30c6c58caa347c691e4f
Nils Toularastel <n.toularastel@cyberg.fr>
2019-11-13 14:25:06 +0100

    build(permissions): add permissions needed

--- android/src/main/java/fr/archeocodix/wifidirect/WifiDirect.java
+++ android/src/main/java/fr/archeocodix/wifidirect/WifiDirect.java
@@ -1,12 +1,23 @@
 package fr.archeocodix.wifidirect;
 
+import android.Manifest;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 
-@NativePlugin()
+@NativePlugin(
+        permissions = {
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.CHANGE_NETWORK_STATE,
+                Manifest.permission.INTERNET,
+                Manifest.permission.ACCESS_NETWORK_STATE
+        }
+)
 public class WifiDirect extends Plugin {
 
     @PluginMethod()
