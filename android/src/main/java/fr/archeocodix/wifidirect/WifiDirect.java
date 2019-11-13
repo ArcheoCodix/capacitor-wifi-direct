4567b19c74807a9fad1d213731d9f54665066b31
Nils Toularastel <n.toularastel@cyberg.fr>
2019-11-13 14:26:51 +0100

    feat(WifiDirect): start implementation

--- android/src/main/java/fr/archeocodix/wifidirect/WifiDirect.java
+++ android/src/main/java/fr/archeocodix/wifidirect/WifiDirect.java
@@ -1,6 +1,11 @@
 package fr.archeocodix.wifidirect;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Looper;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
@@ -20,6 +25,25 @@ import com.getcapacitor.PluginMethod;
 )
 public class WifiDirect extends Plugin {
 
+    WifiP2pManager manager;
+    WifiP2pManager.Channel channel;
+    BroadcastReceiver receiver;
+
+    Context context;
+    IntenteFilter intenteFilter;
+
+    public WifiDirect() {
+        if (!hasRequiredPermissions()) {
+            pluginRequestAllPermissions();
+        }
+
+        this.context = getContext();
+
+        this.manager = (WifiP2pManager) this.context.getSystemService(Context.WIFI_P2P_SERVICE);
+        this.channel = manager.initialize(this.context, this.context.getMainLooper(), null);
+        this.receiver = new WiFiDirectBroadcastReceiver(this.manager, this.channel, this);
+    }
+
     @PluginMethod()
     public void echo(PluginCall call) {
         String value = call.getString("value");
@@ -27,5 +51,15 @@ public class WifiDirect extends Plugin {
         JSObject ret = new JSObject();
         ret.put("value", value);
         call.success(ret);
+    }
+
+    @Override
+    protected void handleOnResume() {
+        super.handleOnResume();
+    }
+
+    @Override
+    protected void handleOnPause() {
+        super.handleOnPause();
     }
 }
