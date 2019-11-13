b8fc66bebb4da97929cc150236d7cfce28b2c628
Nils Toularastel <n.toularastel@cyberg.fr>
2019-11-13 14:26:07 +0100

    feat(WiFiDirectBroadcastReceiver): add class
    to manage wifi direct by extends BroadcastReceiver

---
+++ android/src/main/java/fr/archeocodix/wifidirect/WiFiDirectBroadcastReceiver.java
@@ -0,0 +1,40 @@
+package fr.archeocodix.wifidirect;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.util.Log;
+
+public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
+    private WifiP2pManager mManager;
+    private Channel mChannel;
+    private WifiDirect mWifiDirect;
+
+    public WiFiDirectBroadcastReceiver(WifiP2pManager manager, Channel channel, WifiDirect wifiDirect) {
+        this.mManager = manager;
+        this.mChannel = channel;
+        this.mWifiDirect = wifiDirect;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                Log.i("wifi state", "enabled");
+            } else {
+                Log.i("wifi state", "not enabled");
+            }
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            // Call WifiP2pManager.requestPeers() to get a list of current peers
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            // Respond to new connection or disconnections
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            // Respond to this device's wifi state changing
+        }
+    }
+}
