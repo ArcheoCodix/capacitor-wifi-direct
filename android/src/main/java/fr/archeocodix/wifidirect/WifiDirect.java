e4c1bf9f54068ed2f9c0536601dcd34b0a763891
Archeocodix <b0769@hotmail.fr>
2019-11-17 13:38:09 +0100

    feat(): get peers on discover & return array to plugin

--- android/src/main/java/fr/archeocodix/wifidirect/WifiDirect.java
+++ android/src/main/java/fr/archeocodix/wifidirect/WifiDirect.java
@@ -4,17 +4,32 @@ import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
-import android.os.Looper;
 import android.util.Log;
 
+import com.getcapacitor.JSArray;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 
-@NativePlugin()
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+@NativePlugin(
+        permissions={
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.CHANGE_NETWORK_STATE,
+                Manifest.permission.INTERNET,
+                Manifest.permission.ACCESS_NETWORK_STATE,
+        }
+)
 public class WifiDirect extends Plugin {
 
     WifiP2pManager manager;
@@ -24,11 +39,12 @@ public class WifiDirect extends Plugin {
     Context context;
     IntentFilter intentFilter;
 
+    // Call WifiP2pManager.requestPeers() to get a list of current peers
+    WifiP2pManager.PeerListListener peerListListener;
+
     @Override
     public void load() {
         super.load();
-
-        Log.i("Load", "start");
 
         if (!hasRequiredPermissions()) {
             pluginRequestAllPermissions();
@@ -49,36 +65,61 @@ public class WifiDirect extends Plugin {
 
     @PluginMethod()
     public void discoverPeers(final PluginCall call) {
-        Log.i("DISCOVER", "start");
+        peerListListener = new WifiP2pManager.PeerListListener() {
+            @Override
+            public void onPeersAvailable(WifiP2pDeviceList wifiP2pDeviceList) {
+                Iterator<WifiP2pDevice> list = wifiP2pDeviceList.getDeviceList().iterator();
+                JSArray deviceArray = new JSArray();
+
+                for (; list.hasNext();) {
+                    WifiP2pDevice device = list.next();
+                    JSObject obj = new JSObject();
+
+                    obj.put("deviceAddress", device.deviceAddress);
+                    obj.put("deviceName", device.deviceName);
+                    obj.put("primaryDeviceType", device.primaryDeviceType);
+                    obj.put("secondaryDeviceType", device.secondaryDeviceType);
+                    obj.put("status", device.status);
+
+                    deviceArray.put(obj);
+                }
+
+                JSObject ret = new JSObject();
+
+                ret.put("devices", deviceArray);
+
+                call.success(ret);
+            }
+        };
+
         manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
             @Override
             public void onSuccess() {
-                JSObject ret = new JSObject();
-                ret.put("enabled", true);
-                call.success(ret);
+
             }
 
             @Override
             public void onFailure(int reasonCode) {
-                JSObject ret = new JSObject();
-                ret.put("enabled", false);
-                ret.put("reasonCode", reasonCode);
                 call.reject("error - reason code : " + reasonCode);
             }
         });
     }
 
+    protected void sendConnectionState(boolean isWifiEnabled) {
+        JSObject state = new JSObject();
+        state.put("isEnabled", isWifiEnabled);
+        notifyListeners("wifiState", state);
+    }
+
     @Override
     protected void handleOnResume() {
         super.handleOnResume();
-        Log.i("resume", "on resume");
         context.registerReceiver(receiver, intentFilter);
     }
 
     @Override
     protected void handleOnPause() {
         super.handleOnPause();
-        Log.i("pause", "on pause");
         context.unregisterReceiver(receiver);
     }
 }
