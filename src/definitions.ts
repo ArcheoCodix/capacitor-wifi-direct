dafb055586e5554cada2ddac3133b42965562636
Archeocodix <b0769@hotmail.fr>
2019-11-17 13:36:25 +0100

    feat(): add device interface and status

--- src/definitions.ts
+++ src/definitions.ts
@@ -4,6 +4,23 @@ declare module "@capacitor/core" {
   }
 }
 
+export enum DeviceStatus {
+    Available = 3,
+    Connected = 0,
+    Failed = 2,
+    Invited = 1,
+    Unavailable = 4
+}
+
+export interface WifiP2pDevice {
+    deviceAddress: string;
+    deviceName: string;
+    primaryDeviceType: string;
+    secondaryDeviceType: string;
+    status: DeviceStatus;
+}
+
 export interface WifiDirectPlugin {
-  discoverPeers(options: { value: string }): Promise<any>;
+  discoverPeers(): Promise<{ devices: WifiP2pDevice[] }>;
+  addListener(eventName: 'wifiState', listener: EventListener<{ devices: WifiP2pDevice[] }>): void;
 }
