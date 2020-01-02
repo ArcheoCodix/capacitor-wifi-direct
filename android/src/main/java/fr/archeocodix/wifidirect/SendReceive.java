3bf37db37c4def46e4844a3521b5887b68cddb45
Nils Toularastel <n.toularastel@cyberg.fr>
2020-01-02 14:33:48 +0100

    feat(SendReceive): add class to manage send read

---
+++ android/src/main/java/fr/archeocodix/wifidirect/SendReceive.java
@@ -0,0 +1,48 @@
+package fr.archeocodix.wifidirect;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class SendReceive extends Thread {
+    private Socket socket;
+    private InputStream inputStream;
+    private OutputStream outputStream;
+
+    public SendReceive(Socket skt) {
+        socket = skt;
+
+        try {
+            inputStream = socket.getInputStream();
+            outputStream = socket.getOutputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        byte[] buffer = new byte[1024];
+        int bytes;
+
+        while (socket != null) {
+            try {
+                bytes = inputStream.read(buffer);
+                if (bytes > 0) {
+//                    handler.obtainMessage(MESSAGE_READ, bytes, -1, buffer).sendToTarget();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void write(byte[] bytes) {
+        try {
+            outputStream.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
